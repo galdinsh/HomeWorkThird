64f67fd9b2b0fbbf0aa5582467cc731322a36803
Vladislav <galdinsh77@mail.ru>
2023-03-13 15:51:19 +0500

    Задача №13

---
+++ Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n > 999 )
+{
+    n = n / 10;
+}
+if (n<100)
+Console.WriteLine("Третьего числа нет");
+else
+Console.WriteLine(n % 10);
