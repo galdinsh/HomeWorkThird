e18546539682ecb4e7a95dac5da374a5463d08c7
Vladislav <galdinsh77@mail.ru>
2023-03-13 16:06:43 +0500

    Задача №15

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите день недели(от 1 до 7): ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели(от 1 до 7): ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n >= 1 && n <= 5)
+    Console.WriteLine("Это будни");
+else
+    Console.WriteLine("Это выходной");
