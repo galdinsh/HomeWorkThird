b853fc9022491a884be0438ccc6e589dd68226d3
Vladislav <galdinsh77@mail.ru>
2023-03-13 15:33:15 +0500

    Задача №10

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿    Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n<100 || n>1000)
+    Console.WriteLine("Вы ввели не 3-х значное число"); 
+else    
+Console.WriteLine(n / 10 % 10);
